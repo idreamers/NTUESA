d4d09b64da2bad12af722a8ed793642109a4f3aa
idreamers <idreamers@dreamtech.tw>
2017-04-05 07:52:24 +0800

    NTUESA  Basic Page Test

---
+++ vote_23rd/footer.php
@@ -0,0 +1,11 @@
+<center>
+    <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+    <img alt="創用 CC 授權條款" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" >
+</a>
+<br />
+<h6>國北零時校園 NTUE@e0u<br />
+國北教 教政一 
+<a xmlns:cc="http://creativecommons.org/ns#" href="https://www.facebook.com/dreammner.li" property="cc:attributionName" rel="cc:attributionURL">Rick Li </a>製作。
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0 </a> 。<br />
+</h6>
+</center>
